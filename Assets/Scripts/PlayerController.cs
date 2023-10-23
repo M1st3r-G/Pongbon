bef33ddd3faa009260f17be26c45b46e33d4fbdd
MisterG <g.haegerbaeumer@gmx.de>
2023-10-23 17:45:17 +0200

    Split InputSystem for 2 Players

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;
     private readonly float speed = 3;
+    public bool isPlayer1;
 
     private void Awake()
     {
@@ -12,6 +13,6 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rb.velocity = Input.GetAxisRaw("Vertical") * speed * Vector3.up;
+        rb.velocity = Input.GetAxisRaw(isPlayer1 ? "Player1" : "Player2") * speed * Vector3.up;
     }
 }
