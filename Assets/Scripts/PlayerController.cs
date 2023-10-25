c7a55e05688dbe4d61b27df3ee378e6b03df24d5
MisterG <g.haegerbaeumer@gmx.de>
2023-10-25 13:02:23 +0200

    Kommentieren und Strukturieren des Codes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,17 +2,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //InnerComponentRefrence
     private Rigidbody2D rb;
-    private readonly float speed = 3;
-    public bool isPlayer1;
+    //Outer Params
+    [SerializeField] private static float speed = 3;    //Static, damit für beide Spieler Gleich
+    [SerializeField] private bool isPlayer1;
 
     private void Awake()
     {
+        // Initialisieren der InnerComponentRefrences
         rb = GetComponent<Rigidbody2D>();
     }
 
     private void FixedUpdate()
     {
+        // Wenn Spieler 1, Player1 Axe auswerten, sonst Player2 und dementsprechend Bewegen
         rb.velocity = Input.GetAxisRaw(isPlayer1 ? "Player1" : "Player2") * speed * Vector3.up;
     }
 }
